f60dab06c80a7c47d202dc9aa4759e82bee4f630
pyaesone143 <xieh6264@gmail.com>
2026-09-10 13:32:02 +0630

    Initialize default user on application startup
    Added CommandLineRunner bean to initialize a default user if not present in the database.

--- Backend/src/main/java/com/petfeeder/pet_feeder_backend/PetFeederBackendApplication.java
+++ Backend/src/main/java/com/petfeeder/pet_feeder_backend/PetFeederBackendApplication.java
@@ -1,8 +1,10 @@
 package com.petfeeder.pet_feeder_backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableScheduling
@@ -11,5 +13,24 @@ public class PetFeederBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PetFeederBackendApplication.class, args);
 	}
+	 @Bean
+    CommandLineRunner initUsers(UserRepository userRepository) {
+        return args -> {
+
+            if (userRepository.findByEmail("pyae@gmail.com").isEmpty()) {
+
+                User user = new User();
+
+                user.setName("Pyae Sone Khin");
+                user.setEmail("pyae@gmail.com");
+                user.setPassword("pyaesonekhin");
+            
+
+                userRepository.save(user);
+
+                System.out.println(" user created!");
+            }
+        };
+    }
 
 }
